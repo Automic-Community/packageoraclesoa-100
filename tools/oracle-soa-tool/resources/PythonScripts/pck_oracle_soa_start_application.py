a6ab57d3a8757e59165af7e91821e6fc4c4ddace
Tran-Quang, Huy <tqh@automic.com>
2016-03-22 09:40:00 +0700

    new change of start application

---
+++ tools/oracle-soa-tool/resources/PythonScripts/pck_oracle_soa_start_application.py
@@ -0,0 +1,283 @@
+#pck_weblogic_utils START
+# remove chars if first char or last one are commas
+def rf_trim_comma(str):
+    if str[:1] == ',':
+      str = str[1:len(str)]
+
+    if str[len(str) - 1:] == ',':
+      str = str[:len(str) - 1]
+
+    return str
+
+# remove all chars, spaces if they are first or last chars
+def rf_prep_str(val):
+    if val == None:
+        return None
+
+    i = len(val)
+    if i == 0:
+        return None
+
+    retval = val.strip()
+    retval = rf_trim_comma(retval)
+
+    j = len(retval)
+
+    if i != j:
+        retval = rf_prep_str(retval)
+
+    return retval
+
+# 'servername': 'abc', 'nodename': 'abc'
+def rf_prep_svrs_nodes(str1, str2):
+    arr1 = rf_prep_svrs(str1)
+    arr2 = rf_prep_svrs(str2)
+
+    if len(arr1) != len(arr2):
+        raise 'Unequal no. of elements in each string array'
+    else:
+        retarr = []
+        for item1, item2 in zip(arr1, arr2):
+            # node_server = {\nodename\: item2.strip(), \servername\: item1.strip()}
+            node_server = {'nodename': item2.strip(), 'servername': item1.strip()}
+            retarr.append(node_server)
+        return retarr
+
+# Split string by comma, return array
+def rf_prep_svrs(str1):
+    if str1 == None:
+        return None
+    if len(str1) == 0:
+        return None
+
+    str1 = rf_prep_str(str1)
+    if str1 == None:
+        return []
+    arr1 = str1.split(',');
+
+    retarr = []
+    for item1 in arr1:
+       if len(item1.strip()) > 0:
+         retarr.append(item1.strip())
+
+    return retarr
+
+# remove all space char(which is before or after comma char) in comma delimited string
+def rf_prep_comma_delim_str(str1):
+    if str1 == None:
+        return None
+    if len(str1) == 0:
+        return None
+    retarr = rf_prep_svrs(str1)
+    retstr = ','.join(retarr)
+    return retstr
+
+# check str are yes/true or no/false
+def to_t_f(str):
+    if str.lower() == 'yes' or str.lower() == 'true':
+       str = 'true'
+    elif str.lower() == 'no' or str.lower() == 'false':
+       str = 'false'
+    return str
+
+# return True or False when input string is yes/true or no/false
+def to_boolean(input):
+    if input.lower() == 'yes' or input.lower() == 'true':
+       return True
+    elif input.lower() == 'no' or input.lower() == 'false':
+       return False
+    elif input == '':
+        return False
+    raise "Invalid boolean string: " + input
+
+# return string of characters in ASCII code for integer iterator arr
+def byte_array_to_string(arr):
+     str = ''
+     for b in arr:
+       str += chr(b)
+     return str
+
+# write iterator to file, default separator is double pipes
+def writeReport(m, filePath, separator="||"):
+     report_file = open(filePath, 'w')
+     for list in m.items():
+       str = "%s%s%s\n" % (list[0], separator, list[1])
+       report_file.write(str)
+     report_file.close()
+
+
+def checkResourceExists(name, type):
+    if getMBean('/' + type + 's/' + name) is None:
+        return false
+    else:
+        return true
+
+
+def checkAppExistsInAppDeployments(name):
+  found = false
+  try:
+    cd('/')
+    for app in cmo.getAppDeployments():
+      appName = app.getName()
+      cd('/AppDeployments/' + appName)
+      applicationName = get('ApplicationName')
+      if  applicationName == name:
+        found = true
+        break
+  except Exception:
+    return found
+  return found
+
+def checkAppExists(name,version,targets):
+  """ Check application exist in specific version and at specific targets
+      Keyword arguments:
+      name -- application name
+      version -- application version identifier (declared in manifest).
+                 If blank, the active version will be used
+      targets -- deployment targets which should be checked
+  """
+  found = false
+  try:
+    appBean = None
+    if (version is None or version.strip() == ''):
+      appBean = getAppMBeanCore(name, version, True)
+    else:
+      appBean = getAppMBean(name, version)
+    if appBean is None:
+      return found
+    # if (targets is None or targets.strip() == ''):
+      # cd('/')
+      # targets = cmo.getAdminServerName()
+      # print 'Input targets is blank, using \"'+targets+'\"" target by default!'
+    for target in appBean.getTargets():
+      found = true
+      print 'Application \"'+name+'\" was deployed to target \"'+target.getName()+'\".'
+      break
+  except Exception:
+    return found
+  return found
+
+def isActiveVersion(appNameWithVersion):
+    domainRuntime()
+    cd('/AppRuntimeStateRuntime/AppRuntimeStateRuntime')
+    isActive = cmo.isActiveVersion(appNameWithVersion)
+
+    #Go back to server config
+    serverConfig()
+    if(isActive == 1):
+        print appNameWithVersion + ' is the active version'
+        return True
+    else:
+        print appNameWithVersion + ' is not the active version'
+        return False
+
+
+def getAppMBean(name, version):
+    return getAppMBeanCore(name, version, False)
+
+def getAppMBeanCore(name, version, useActiveVersion):
+    """Returns an application object with the provided version.
+        Keyword arguments:
+        name -- application name
+        version -- application version identifier (declared in manifest). If blank, the first version found is returned
+        useActiveVersion -- if true, version argument is ignored, only the active version is returned (an application may have 0 or 1 active version)
+    """
+    print 'getApplication: name = %s, version = %s, useActiveVersion = %s' % (name, version, useActiveVersion)
+    if(useActiveVersion is True):
+        version = ''
+
+    mb = None
+    try:
+        cd('/')
+        for app in cmo.getAppDeployments():
+          appNameWithVersion = app.getName() #Format of appNameWithVersion is appName#version
+          cd('/AppDeployments/' + appNameWithVersion)
+          applicationName = get('ApplicationName')
+          appVersion = get('VersionIdentifier')
+          if  applicationName == name or appNameWithVersion == name:
+            if  version == '':
+                if(useActiveVersion is False):
+                    mb = getMBean('/AppDeployments/' + appNameWithVersion)
+                    break
+                else:
+                    if(isActiveVersion(appNameWithVersion) is True):
+                        mb = getMBean('/AppDeployments/' + appNameWithVersion)
+                        break
+            else:
+                if appVersion == version:
+                    mb = getMBean('/AppDeployments/' + appNameWithVersion)
+                    break
+    except Exception:
+        return mb
+    return mb
+
+#pck_weblogic_utils END
+
+import sys, string, os, os.path, time, traceback, shutil
+execfile(sys.argv[])
+print "Execute python script with WLST"
+
+inputAppName = sys.argv[4]
+inputFailFlag = sys.argv[6].lower()
+"&UC4RB_ORACLE_SOA_APP_NAME#" "&UC4RB_ORACLE_SOA_REVISION#" "&UC4RB_ORACLE_SOA_FAIL_RUNNING#" "&UC4RB_ORACLE_SOA_LABEL#" "&UC4RB_ORACLE_SOA_PARTITION#
+def printInputs():
+    print 'User: ' + sys.argv[1]
+    print 'Host and port: ' + sys.argv[3]
+    print 'Composite name: ' + inputAppName
+    print 'Revision: ' + inputAppName
+    print 'Label: ' + inputAppName
+    print 'Partition: ' + inputAppName
+    print 'Fail Flag: ' + inputFailFlag
+    
+printInputs()
+
+connect(sys.argv[1].strip(), sys.argv[2].strip(), sys.argv[3].strip()) 
+
+depl_apps = ls('/AppDeployments/')
+app_arr = depl_apps.replace('dr--   ', '').split('\n')  
+appName = rf_prep_str(inputAppName)    
+
+'''
+if not checkAppExists(appName,'',''):  
+     raise 'Application %s does not exist, Job failed' % inputAppName 
+  
+found_active_app = ''
+cd('domainRuntime:/AppRuntimeStateRuntime/AppRuntimeStateRuntime')    
+intendedState = cmo.getIntendedState(appName)    
+if intendedState == 'STATE_ACTIVE':    
+   found_active_app = appName     
+   print 'Found active app:' + found_active_app      
+if found_active_app == '':      
+   i = 0  
+   for app in app_arr:  
+       app_name = app_arr[i].strip()  
+       print 'app name:' + app_name   
+       if (len(app_name) > len(appName)) and   (app_name[0:len(appName) + 1] == appName + '#'): 
+            intendedState = cmo.getIntendedState(app_name)   
+            print 'intendedState for ' + app_name + ' is '    
+            print  intendedState    
+            if intendedState == 'STATE_ACTIVE':    
+                found_active_app = app_name       
+                print 'Found active app:' + found_active_app     
+                break                            
+       i = i + 1  
+
+try:
+
+   failed = 1
+   if found_active_app != '':
+       if inputFailFlag == "no":           
+           print 'Application ' + found_active_app + ' is already active, exit'   
+           disconnect('true')
+           sys.exit()
+       else:
+           raise 'Application is already active, Job failed'
+'''
+try:
+   progress = sca_startComposite('vvnarasoa02','7003','weblogic','ventum2011','VacationRequest','1.0')
+   print 'Application started.'
+   failed = 0
+finally:    
+   if failed:    
+      print 'Application failed to start.'    
+   disconnect('true')    
